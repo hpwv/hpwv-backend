c4e5866f41e66c0694c6554e80d5df3b2317ca38
Maximilian Büttner <maximilian.buettner@westernacher.com>
2022-01-10 17:43:49 +0100

    add socket io handler;

---
+++ src/socket/handler.js
@@ -0,0 +1,31 @@
+const {Server} = require('socket.io'),
+    Client = require('./Client'),
+    {nanoid} = require('nanoid'),
+    {logger} = require('../log/logger');
+
+let io,
+    clients = new Set();
+
+const initializeSocketIo = (server) => {
+    io = new Server(server);
+
+    io.on('connection', (socket) => {
+        const client = new Client(nanoid(), socket)
+        clients.add(client);
+        logger.info(`Client with id ${client.id} connected.`);
+
+        socket.on('disconnect', (reason) => {
+            logger.info(`Client with id ${client.id} disconnected, reason: ${reason}`);
+            clients.delete(client);
+        });
+    });
+}
+
+const closeAllClientConnections = async () => {
+    return Promise.all(Array.from(clients).map(it => it.disconnect()));
+}
+
+module.exports = {
+    initializeSocketIo,
+    closeAllClientConnections
+}
